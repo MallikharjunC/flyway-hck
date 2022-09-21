81ca81ba144b071e503f4e460b831368abf45373
arjun.mannambeth <arjun.mannambeth@nutanix.com>
2022-09-21 18:00:33 +0530

    Migration updates

--- src/test/java/com/nutanix/db/migration/FlywayMigration.java
+++ src/test/java/com/nutanix/db/migration/FlywayMigration.java
@@ -2,17 +2,22 @@ package com.nutanix.db.migration;
 
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.internal.info.MigrationInfoDumper;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
-public class FlywayMigration    {
+public class FlywayMigration {
 
-    private static Properties properties=new Properties();
+    private static Properties properties = new Properties();
+    private String activity;
+    private Flyway flyway;
+
     static {
-        String filePath = System.getProperty("configFileName","src/test/resources/conf/flyway.conf");
+        String filePath = System.getProperty("configFileName", "src/test/resources/conf/flyway.conf");
         System.out.println("Reading config from : " + filePath);
         File file = new File(filePath);
         try {
@@ -22,20 +27,41 @@ public class FlywayMigration    {
         }
     }
 
-    @Test
-    private void migrateDataBase(){
+    @BeforeSuite
+    public void configure() {
         System.out.println("Connecting to : " + properties.getProperty("url"));
-        Flyway flyway=Flyway.configure()
+        String activity = System.getProperty("activity", "migrate");
+        Flyway flyway = Flyway.configure()
                 .licenseKey(properties.getProperty("licenseKey"))
                 .dataSource(properties.getProperty("url"),
                         properties.getProperty("user"),
                         properties.getProperty("password"))
                 .defaultSchema(properties.getProperty("schema"))
                 .load();
+        System.out.println("Version history before " + activity + " :\n" +
+                MigrationInfoDumper.dumpToAsciiTable(flyway.info().all()));
+    }
 
-        System.out.println(MigrationInfoDumper.dumpToAsciiTable(flyway.info().all()));
-        flyway.undo();
-        System.out.println(MigrationInfoDumper.dumpToAsciiTable(flyway.info().all()));
+    @Test
+    private void migrateDataBase() {
+        System.out.println("Starting activity : " + activity);
+
+        if (activity.equalsIgnoreCase("migrate")) {
+            flyway.migrate();
+        } else if (activity.equalsIgnoreCase("undo")) {
+            flyway.undo();
+        } else {
+            System.err.println("Invalid activity : " + activity);
+            System.err.println("Please use valid activity name [migrate, undo]");
+        }
+    }
+
+    @AfterSuite
+    public void validate(){
+        System.out.println("Version history after " + activity + " :\n" +
+                MigrationInfoDumper.dumpToAsciiTable(flyway.info().all()));
+        System.out.println("Performing migration validation");
+        flyway.validate();
     }
 
 }
