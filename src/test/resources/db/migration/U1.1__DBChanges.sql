9871cae9353394c88b9c2a1fe6eca1b1aa0b1341
arjun.mannambeth <arjun.mannambeth@nutanix.com>
2022-09-23 06:31:13 +0530

    Db Changes

---
+++ src/test/resources/db/migration/U1.1__DBChanges.sql
@@ -0,0 +1,5 @@
+DROP TABLE AccountInfo;
+
+ALTER TABLE UserInfo DROP email;
+
+ALTER TABLE UserInfo DROP email;
---
+++ src/test/resources/db/migration/V1.1__DBChanges.sql
@@ -0,0 +1,9 @@
+CREATE TABLE AccountInfo (
+    id INT NOT NULL PRIMARY KEY,
+    acc_name VARCHAR NOT NULL,
+);
+
+ALTER TABLE UserInfo ADD email VARCHAR;
+
+ALTER TABLE SellerInfo ADD email VARCHAR;
+
