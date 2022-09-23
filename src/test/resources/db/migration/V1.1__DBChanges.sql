a3dd4c001d82bae276b540f631767be7e29bf45a
arjun.mannambeth <arjun.mannambeth@nutanix.com>
2022-09-23 09:23:43 +0530

    Db Changes

--- src/test/resources/db/migration/V1.1__DBChanges.sql
+++ src/test/resources/db/migration/V1.1__DBChanges.sql
@@ -7,5 +7,5 @@ ALTER TABLE UserInfo ADD email VARCHAR;
 
 ALTER TABLE SellerInfo ADD email VARCHAR;
 
-UPDATE UserInfo set email="john@abc.com";
+UPDATE UserInfo set email='john@abc.com' where name='John';
 
