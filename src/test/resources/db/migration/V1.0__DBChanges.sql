47cb8c4240e166ba8b2cdc22c7c9023c436185f3
arjun.mannambeth <arjun.mannambeth@nutanix.com>
2022-09-23 09:03:06 +0530

    Db Changes

--- src/test/resources/db/migration/V1.0__DBChanges.sql
+++ src/test/resources/db/migration/V1.0__DBChanges.sql
@@ -8,9 +8,9 @@ CREATE TABLE SellerInfo (
     org VARCHAR NOT NULL
 );
 
-INSERT into UserInfo (id,name) values (1,"John");
-INSERT into UserInfo (id,name) values (1,"Mike");
+INSERT into UserInfo (id,name) values (1,'John');
+INSERT into UserInfo (id,name) values (1,'Mike');
 
 
-INSERT into UserInfo (id,org) values (1,"ABC");
-INSERT into UserInfo (id,org) values (1,"XYZ");
+INSERT into UserInfo (id,org) values (1,'ABC');
+INSERT into UserInfo (id,org) values (1,'XYZ');
